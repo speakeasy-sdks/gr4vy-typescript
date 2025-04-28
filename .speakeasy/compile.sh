5ce594303e767dd16f0157f32a4a21e20fe0f01c
Cristiano Betta <cbetta@gr4vy.com>
2025-04-28 19:18:15 +0100

    chore: Update sed command

--- .speakeasy/compile.sh
+++ .speakeasy/compile.sh
@@ -12,6 +12,6 @@ else
 fi
 
 # Update code samples to use the global syntax
-find . -type f -name '*.md' | xargs sed '' 's/bearerAuth: process.env\["GR4VY_BEARER_AUTH"\] ?? "",/server: "sandbox",\n  id: "example",\n  bearerAuth: withToken({\n    privateKey: fs.readFileSync("private_key.pem", "utf8"),\n  }),/g'
-find . -type f -name '*.md' | xargs sed '' 's/import { Gr4vyCore } from "@gr4vy\/sdk\/core\.js";/import { Gr4vyCore } from "@gr4vy\/sdk\/core\.js";\nimport { withToken } from "@gr4vy\/sdk\/lib\/auth\.js";/g'
+find . -type f -name '*.md' | xargs sed 's/bearerAuth: process.env\["GR4VY_BEARER_AUTH"\] ?? "",/server: "sandbox",\n  id: "example",\n  bearerAuth: withToken({\n    privateKey: fs.readFileSync("private_key.pem", "utf8"),\n  }),/g'
+find . -type f -name '*.md' | xargs sed 's/import { Gr4vyCore } from "@gr4vy\/sdk\/core\.js";/import { Gr4vyCore } from "@gr4vy\/sdk\/core\.js";\nimport { withToken } from "@gr4vy\/sdk\/lib\/auth\.js";/g'
 
