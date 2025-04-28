bb37d6cf6365f9f1622d74c77e38b7cfc613c688
Cristiano Betta <cbetta@gr4vy.com>
2025-04-28 19:23:40 +0100

    chore: Update sed switch

--- .speakeasy/compile.sh
+++ .speakeasy/compile.sh
@@ -5,13 +5,12 @@ set -e
 npm install
 npm run build
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  alias sed='sed -i ""'
-else
-  alias sed='sed -i'
-fi
-
 # Update code samples to use the global syntax
-find . -type f -name '*.md' | xargs sed 's/bearerAuth: process.env\["GR4VY_BEARER_AUTH"\] ?? "",/server: "sandbox",\n  id: "example",\n  bearerAuth: withToken({\n    privateKey: fs.readFileSync("private_key.pem", "utf8"),\n  }),/g'
-find . -type f -name '*.md' | xargs sed 's/import { Gr4vyCore } from "@gr4vy\/sdk\/core\.js";/import { Gr4vyCore } from "@gr4vy\/sdk\/core\.js";\nimport { withToken } from "@gr4vy\/sdk\/lib\/auth\.js";/g'
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    find . -type f -name '*.md' | xargs sed -i '' 's/bearerAuth: process.env\["GR4VY_BEARER_AUTH"\] ?? "",/server: "sandbox",\n  id: "example",\n  bearerAuth: withToken({\n    privateKey: fs.readFileSync("private_key.pem", "utf8"),\n  }),/g'
+    find . -type f -name '*.md' | xargs sed -i '' 's/import { Gr4vyCore } from "@gr4vy\/sdk\/core\.js";/import { Gr4vyCore } from "@gr4vy\/sdk\/core\.js";\nimport { withToken } from "@gr4vy\/sdk\/lib\/auth\.js";/g'
+else
+    find . -type f -name '*.md' | xargs sed -i 's/bearerAuth: process.env\["GR4VY_BEARER_AUTH"\] ?? "",/server: "sandbox",\n  id: "example",\n  bearerAuth: withToken({\n    privateKey: fs.readFileSync("private_key.pem", "utf8"),\n  }),/g'
+    find . -type f -name '*.md' | xargs sed -i 's/import { Gr4vyCore } from "@gr4vy\/sdk\/core\.js";/import { Gr4vyCore } from "@gr4vy\/sdk\/core\.js";\nimport { withToken } from "@gr4vy\/sdk\/lib\/auth\.js";/g'
+fi
 
